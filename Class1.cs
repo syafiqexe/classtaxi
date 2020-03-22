53f1b0313842d1ba9acde43dd8a17ef21d25322c
syafiqexe <ahsyafiq12@gmail.com>
2020-03-22 08:57:38 +0700

    taxi

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tugas_class_dan_object
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            taxi1 taxi = new taxi1();
+
+            taxi.drivername = "joko";
+            taxi.onduty = true;
+            taxi.numpassenger = 10;
+
+            taxi.taxiinfo();
+            taxi.pickuppassenger();
+            taxi.dropoffpassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace tugas_class_dan_object
+{
+    public class taxi1
+    {
+        public string drivername;
+        public bool onduty;
+        public int numpassenger;
+
+        public void taxiinfo()
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("| driver name  :{0}|", drivername);
+            Console.WriteLine("|on duty       : {0} |", onduty);
+            Console.WriteLine("|number of passenger : {0} |", numpassenger);
+
+        }
+
+        public void pickuppassenger()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("{0}sedang menjemput penumpang", drivername);
+        }
+        public void dropoffpassenger()
+        {
+            Console.WriteLine("{0}selesai mengantar penumpang", drivername);
+        }
+
+
+
+    }
+}
